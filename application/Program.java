4bf0a96db25662ca42509ec8c0964141df100b52
Bruno_Neves <nevesbruno814@gmail.com>
2025-01-14 23:31:35 -0400

    sallary  end class

--- application/Program.java
+++ application/Program.java
@@ -1,4 +1,18 @@
 package application;
 
+import entities.Department;
+import entities.Seller;
+
+import java.time.LocalDate;
+
 public class Program {
+
+    public static void main(String[] args) {
+        Department dp = new Department(1, "Books");
+        Seller seller = new Seller(21,"bob", "bob@gamil", LocalDate.now(), 3000.0, dp);
+
+        System.out.println(seller);
+    }
+
+
 }
