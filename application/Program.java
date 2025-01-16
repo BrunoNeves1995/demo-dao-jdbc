efa3f39557ca80cba1616aac3aeab06621924bab
Bruno_Neves <nevesbruno814@gmail.com>
2025-01-15 20:00:21 -0400

    findById Implementation

--- application/Program.java
+++ application/Program.java
@@ -10,10 +10,9 @@ import java.time.LocalDate;
 public class Program {
 
     public static void main(String[] args) {
-        Department dp = new Department(1, "Books");
-        Seller seller = new Seller(21,"bob", "bob@gamil", LocalDate.now(), 3000.0, dp);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
